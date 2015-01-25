c615c655ff03e78a8be87cb7144be1abc5316638
Harald Floor Wilhelmsen <haraldfw@gmail.com>
2015-01-26 00:49:10 +0100

    Simple chat client in asp.net/signalr

---
+++ MuncherClient/MuncherClient/MuncherClient/Startup.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.AspNet.SignalR;
+using Microsoft.Owin.Hosting;
+using Owin;
+using Microsoft.Owin.Cors;
+
+[assembly: Microsoft.Owin.OwinStartup(typeof(MuncherBackend.Startup))]
+namespace MuncherBackend
+{
+
+    class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseCors(CorsOptions.AllowAll);
+            app.MapSignalR();
+        }
+    }
+}
